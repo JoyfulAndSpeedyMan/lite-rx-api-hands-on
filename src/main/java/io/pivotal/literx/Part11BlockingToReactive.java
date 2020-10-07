6426e8159e04dce8cf82017d6851cd000437558e
Boy <905991211@qq.com>
2020-10-07 21:05:03 +0800

    Part11 First

--- src/main/java/io/pivotal/literx/Part11BlockingToReactive.java
+++ src/main/java/io/pivotal/literx/Part11BlockingToReactive.java
@@ -7,6 +7,8 @@ import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Scheduler;
 import reactor.core.scheduler.Schedulers;
 
+import java.util.concurrent.Executors;
+
 /**
  * Learn how to call blocking code from Reactive one with adapted concurrency strategy for
  * blocking code that produces or receives data.
@@ -27,14 +29,20 @@ public class Part11BlockingToReactive {
 
 	// TODO Create a Flux for reading all users from the blocking repository deferred until the flux is subscribed, and run it with an elastic scheduler
 	Flux<User> blockingRepositoryToFlux(BlockingRepository<User> repository) {
-		return null;
+		return Flux.defer(()->Flux.fromIterable(repository.findAll()))
+				.subscribeOn(Schedulers.elastic());
 	}
 
 //========================================================================================
 
 	// TODO Insert users contained in the Flux parameter in the blocking repository using an elastic scheduler and return a Mono<Void> that signal the end of the operation
 	Mono<Void> fluxToBlockingRepository(Flux<User> flux, BlockingRepository<User> repository) {
-		return null;
+		return flux
+				.publishOn(Schedulers.elastic())
+				.doOnNext(u-> {
+					repository.save(u);
+				})
+				.then();
 	}
 
 }
